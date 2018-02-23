12a7486abf3a8052f7451455ea71d9813f385d52
Greg Steiert <41132966+gsteiert@users.noreply.github.com>
2018-02-23 23:25:58 +0000

    Creating USBMSD Block Device implementation

--- USBMSD_BD.h
+++ USBMSD_BD.h
@@ -1,4 +1,4 @@
-/* mbed USBMSD_SD Library, for providing file access to SD cards
+/* mbed USBMSD_BD Library, for providing file access to Block Devices
  * Copyright (c) 2008-2010, sford
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -20,19 +20,20 @@
  * THE SOFTWARE.
  */
 
-#ifndef USBMSD_SD_H
-#define USBMSD_SD_H
+#ifndef USBMSD_BD_H
+#define USBMSD_BD_H
 
 #include "mbed.h"
+#include "BlockDevice.h"
 #include "USBMSD.h"
 
 /** Use the SDcard as mass storage device using the USBMSD class
  *
  * @code
  * #include "mbed.h"
- * #include "USBMSD_SD.h"
+ * #include "USBMSD_BD.h"
  *
- * USBMSD_SD sd(p5, p6, p7, p8);
+ * USBMSD_BD sd(p5, p6, p7, p8);
  *
  * int main() {
  *   while(1);
@@ -40,7 +41,7 @@
  *
  * @endcode
  */
-class USBMSD_SD : public USBMSD {
+class USBMSD_BD : public USBMSD {
 public:
 
     /** Create the File System for accessing an SD Card using SPI
@@ -51,36 +52,23 @@ public:
      * @param cs   DigitalOut pin used as SD Card chip select
      * @param name The name used to access the virtual filesystem
      */
-    USBMSD_SD(PinName mosi, PinName miso, PinName sclk, PinName cs);
+    USBMSD_BD(BlockDevice *bd, bd_size_t ssize);
     virtual int disk_initialize();
     virtual int disk_status();
     virtual int disk_read(uint8_t * buffer, uint64_t block_number, uint8_t count);
     virtual int disk_write(const uint8_t * buffer, uint64_t block_number, uint8_t count);
     virtual int disk_sync();
-    virtual uint64_t disk_sectors();
-    
-    virtual uint64_t disk_size(){return _sectors*512;};
+    virtual uint64_t disk_sectors();    
+    virtual uint64_t disk_size();
 
 protected:
-
-    int _cmd(int cmd, int arg);
-    int _cmdx(int cmd, int arg);
-    int _cmd8();
-    int _cmd58();
-    int initialise_card();
-    int initialise_card_v1();
-    int initialise_card_v2();
     
-    int _read(uint8_t * buffer, uint32_t length);
-    int _write(const uint8_t *buffer, uint32_t length);
-    uint64_t _sd_sectors();
     uint64_t _sectors;
+    bd_size_t _ssize;
     
     uint8_t _status;
     
-    SPI _spi;
-    DigitalOut _cs;
-    int cdv;
+    BlockDevice* _bd;
 };
 
 #endif
